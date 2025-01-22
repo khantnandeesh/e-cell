65ed8881c78a95bd9c74a9272ecc91d40c2bb629
khantnandeesh <nandeeshkhant3205@gmail.com>
2025-01-22 15:31:32 +0530

    minor bug/pixel-perfect

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,23 +2,24 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div className=' lg:w-[85%] md:w-[93%]   mt-16 md:mt-11   mx-auto flex gap-9 md:justify-between md:gap-[20px] justify-center flex-wrap '>
+        <div className=' lg:h-[26.73vw] lg:w-[83.54vw]    lg:mt-[3.47vw] md:mt-[6.45vw]    mx-auto flex lg:justify-between   md:justify-between   md:flex-wrap md:gap-[2.419vw]  md:h-[99vw] md:w-[92.87vw]'>
 
-            <div className='h-[46vh] w-[46vh] md:w-[100%] md:h-[45vw] bg-gradient-to-br rounded-lg from-[#3300FF] to-[#FA00FF] shadow-[0_0_4px_4px_rgba(0,0,0,0.25)]' ></div>
-            <div className='h-[46vh]  md:h-[45vw] md:w-[50%] md:flex-1 rounded-md overflow-hidden border-black border-[1px] '>
-                <img src="https://s3-alpha-sig.figma.com/img/f451/3151/c543be76d658fa7cf401125e9cef6f94?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Umhrmf3ve5VwHjU9x-4lqn9VoteG~xmqFFAJ1NFlSAPT2F1K~eCqdTncMldvRuSwe7MGXpMkWCK9BVDlwoLxeKQ8MOk22FzwjwYl~dXpuh2U35tx1RvrkuHzRks0J7qpvRw~3LguA3Xi4mQjmUtCMqf9V7DbbaYO38HIqgj4iJ1JJ6u05G6GZZI5IqORNTSqkpUoRjfh1YuRICXgk79qz0718w~phQ2SE3Bmh1NCvyTjFecnU6ajWd6Bjj-mQ0gqKBCsaTsYA-X9YzyKyRkyCY6r8bDTCU43ejpA3U6bhERIx1CEgfUCQwTsnlKkwvdY0R56TLm38CkTc38cvbgyqQ__" className='h-full  object-cover' alt="image" />
+            <div className='lg:h-[26.73vw] lg:w-[26.73vw] lg:max-w-[26.73vw] lg:max-h-[26.73vw]  md:w-[100%] md:h-[51.74vw] bg-gradient-to-br rounded-lg from-[#3300FF] to-[#FA00FF] shadow-[0_0_4px_4px_rgba(0,0,0,0.25)]' ></div>
+            <div className='lg:h-[26.73vw] lg:w-[26.73vw] lg:max-w-[26.73vw] lg:max-h-[26.73vw]   md:h-[45.29vw] md:w-[45.29vw]  rounded-md overflow-hidden border-black border-[1px] bg-[#F7F7F7]'>
+                <img src="https://s3-alpha-sig.figma.com/img/f451/3151/c543be76d658fa7cf401125e9cef6f94?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Umhrmf3ve5VwHjU9x-4lqn9VoteG~xmqFFAJ1NFlSAPT2F1K~eCqdTncMldvRuSwe7MGXpMkWCK9BVDlwoLxeKQ8MOk22FzwjwYl~dXpuh2U35tx1RvrkuHzRks0J7qpvRw~3LguA3Xi4mQjmUtCMqf9V7DbbaYO38HIqgj4iJ1JJ6u05G6GZZI5IqORNTSqkpUoRjfh1YuRICXgk79qz0718w~phQ2SE3Bmh1NCvyTjFecnU6ajWd6Bjj-mQ0gqKBCsaTsYA-X9YzyKyRkyCY6r8bDTCU43ejpA3U6bhERIx1CEgfUCQwTsnlKkwvdY0R56TLm38CkTc38cvbgyqQ__" className='h-full w-full  object-contain' alt="image" />
             </div>
 
-            <div className='h-[46vh] md:h-[45vw] md:w-[50%] md:flex-1   w-[46vh] bg-[#D9D9D9] rounded-lg p-4 flex justify-center items-center flex-wrap gap-2 shadow-[0_0_2px_4px_rgba(0,0,0,0.25)]'>
-                <div  className='h-[30%] rounded-lg w-[30%] bg-[#00477A]' ></div>
-                <div className='h-[30%] rounded-lg w-[30%] bg-[#00477A]' ></div>
-                <div  className='h-[30%] rounded-lg w-[30%] bg-[#00477A]'></div>
-                <div  className='h-[30%] rounded-lg w-[30%] bg-[#00477A]'></div>
-                <div  className='h-[30%] rounded-lg w-[30%] bg-[#00477A]'></div>
-                <div  className='h-[30%] rounded-lg w-[30%] bg-[#00477A]'></div>
-                <div  className='h-[30%] rounded-lg w-[30%] bg-[#00477A]'></div>
-                <div  className='h-[30%] rounded-lg w-[30%] bg-[#00477A]'></div>
-                <div  className='h-[30%] rounded-lg w-[30%] bg-[#7A0000]'></div>
+            <div className='lg:h-[26.73vw] lg:max-w-[26.73vw] lg:max-h-[26.73vw]  lg:w-[26.73vw] md:w-[45.026vw] md:h-[45.29vw] md:w-max-[45.026vw] md:h-max-[45.29vw]  lg:gap-[0.486vw] md:gap-[0.82vw]  bg-[#D9D9D9] rounded-lg lg:px-[2.465vw] lg:py-[2.396vw] flex justify-center items-center flex-wrap  shadow-[inset_0_0_4px_2px_rgba(0,0,0,0.25)]   md:px-[4.15vw] md:py-[4.06vw]'>
+                <div  className='lg:h-[6.94vw] rounded-2xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]' ></div>
+                <div className='lg:h-[6.94vw] rounded-2xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]' ></div>
+                <div  className='lg:h-[6.94vw] rounded-2xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]'></div>
+                <div  className='lg:h-[6.94vw] rounded-2xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]'></div>
+                <div  className='lg:h-[6.94vw] rounded-2xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]'></div>
+                <div  className='lg:h-[6.94vw] rounded-2xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]'></div>
+             
+                <div  className='lg:h-[6.94vw] rounded-2xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]'></div>
+                <div  className='lg:h-[6.94vw] rounded-2xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]'></div>
+                <div  className='lg:h-[6.94vw] rounded-2xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#7A0000]'></div>
             </div>
         </div>
       )
--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -3,7 +3,7 @@ import 'remixicon/fonts/remixicon.css';
 
 export const Nav = () => {
   return (
-    <div className='w-[100%] md:text-md  h-16 items-center   flex justify-end gap-12 py-2 px-7 text-md font-light tracking-wide  shadow-lg shadow-[0_0_4px_4px_rgba(0,0,0,0.25)]'>
+    <div className='w-[100%] md:text-md  md:h-[7.66vw] lg:h-[3.8vw] items-center   flex justify-end gap-12 py-2 px-7 lg:text-[1.25vw] md:text-[2.42vw] font-light tracking-wide   shadow-[0_0_4px_4px_rgba(0,0,0,0.25)]'>
 
         <div >Home</div>
         <div>Grid</div>
