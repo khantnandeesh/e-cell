541b0e482949d0cb45b40a085cfc88388561f2e5
khantnandeesh <nandeeshkhant3205@gmail.com>
2025-01-22 20:55:25 +0530

    first-page-completed

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+
+    let [currColor,setColor]=useState([false,false,false,false,false,false,false,false]);
+    let [history,setHistory]=useState([]);
+    console.log(history);
+
+
+    async function reset() {
+        // Use for-loop to ensure async operations are executed in order
+        for (let index of history) {
+            // Update the color state for the current index
+            setColor(arr => arr.map((obj, i) => (i === index ? false : obj)));
+    
+            // Wait for 1 second before moving to the next iteration
+            await new Promise((res) => setTimeout(res, 400));
+        }
+
+
+        setHistory([]);
+    }
+
     return (
         <div className=' lg:h-[26.73vw] lg:w-[83.54vw]  mt-[3.5623vw] lg:mt-[3.47vw] md:mt-[6.45vw] gap-[3.18vw]     mx-auto flex  flex-col-reverse  items-center md:flex-row lg:flex-row lg:flex-nowrap   md:justify-between   md:flex-wrap lg:gap-[0]  md:gap-[2.419vw]  md:h-[99vw] md:w-[92.87vw]'>
 
@@ -9,20 +29,76 @@ const Home = () => {
                 <img src="https://s3-alpha-sig.figma.com/img/f451/3151/c543be76d658fa7cf401125e9cef6f94?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Umhrmf3ve5VwHjU9x-4lqn9VoteG~xmqFFAJ1NFlSAPT2F1K~eCqdTncMldvRuSwe7MGXpMkWCK9BVDlwoLxeKQ8MOk22FzwjwYl~dXpuh2U35tx1RvrkuHzRks0J7qpvRw~3LguA3Xi4mQjmUtCMqf9V7DbbaYO38HIqgj4iJ1JJ6u05G6GZZI5IqORNTSqkpUoRjfh1YuRICXgk79qz0718w~phQ2SE3Bmh1NCvyTjFecnU6ajWd6Bjj-mQ0gqKBCsaTsYA-X9YzyKyRkyCY6r8bDTCU43ejpA3U6bhERIx1CEgfUCQwTsnlKkwvdY0R56TLm38CkTc38cvbgyqQ__" className='h-full w-full  object-contain' alt="image" />
             </div>
 
-            <div className='h-[63.61vw] w-[63.61vw]  lg:h-[26.73vw] lg:shrink-0 lg:max-w-[26.73vw] lg:max-h-[26.73vw]  lg:w-[26.73vw] md:w-[45.026vw] md:h-[45.29vw] md:w-max-[45.026vw] md:h-max-[45.29vw] gap-[1.15vw]  lg:gap-[0.486vw]  md:gap-[0.82vw]  bg-[#D9D9D9] rounded-3xl  flex justify-center items-center flex-wrap  shadow-[inset_0_0_4px_4px_rgba(0,0,0,0.25)] px-[5.86vw] py-[5.70vw]   md:px-[4.15vw] md:py-[4.06vw] lg:px-[2.465vw] lg:py-[2.395vw]'>
-                <div  className='h-[16.52vw] w-[16.52vw] lg:h-[6.94vw] rounded-3xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]' ></div>
-                <div className='h-[16.52vw] w-[16.52vw] lg:h-[6.94vw] rounded-3xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]' ></div>
-                <div  className='h-[16.52vw] w-[16.52vw] lg:h-[6.94vw] rounded-3xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]'></div>
-                <div  className='h-[16.52vw] w-[16.52vw] lg:h-[6.94vw] rounded-3xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]'></div>
-                <div  className='h-[16.52vw] w-[16.52vw] lg:h-[6.94vw] rounded-3xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]'></div>
-                <div  className='h-[16.52vw] w-[16.52vw] lg:h-[6.94vw] rounded-3xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]'></div>
-             
-                <div  className='h-[16.52vw] w-[16.52vw] lg:h-[6.94vw] rounded-3xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]'></div>
-                <div  className='h-[16.52vw] w-[16.52vw] lg:h-[6.94vw] rounded-3xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]'></div>
-                <div  className='h-[16.52vw] w-[16.52vw] lg:h-[6.94vw] rounded-3xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#7A0000]'></div>
+            <div     className='h-[63.61vw] w-[63.61vw]  lg:h-[26.73vw] lg:shrink-0 lg:max-w-[26.73vw] lg:max-h-[26.73vw]  lg:w-[26.73vw] md:w-[45.026vw] md:h-[45.29vw] md:w-max-[45.026vw] md:h-max-[45.29vw] gap-[1.15vw]  lg:gap-[0.486vw]  md:gap-[0.82vw]  bg-[#D9D9D9] rounded-3xl  flex justify-center items-center flex-wrap  shadow-[inset_0_0_4px_4px_rgba(0,0,0,0.25)] px-[5.86vw] py-[5.70vw]   md:px-[4.15vw] md:py-[4.06vw] lg:px-[2.465vw] lg:py-[2.395vw]'>
+
+                        <div 
+                        onClick={() => {
+                            setHistory(() => history.indexOf(0) == -1 ? [...history, 0] : history);
+                            setColor(arr => arr.map((obj, i) => (i == 0 ? true : obj)));
+                        }}
+                        className={`h-[16.52vw] w-[16.52vw] lg:h-[6.94vw] rounded-3xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] ${currColor[0] ? 'bg-[#7A0000]' : 'bg-[#00477A]'} transition-all ease-in-out duration-[400ms] delay-[20ms]`}
+                        ></div>
+
+                        <div 
+                        onClick={() => {
+                            setHistory(() => history.indexOf(1) == -1 ? [...history, 1] : history);
+                            setColor(arr => arr.map((obj, i) => (i == 1 ? true : obj)));
+                        }}
+                        className={`h-[16.52vw] w-[16.52vw] lg:h-[6.94vw] rounded-3xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] ${currColor[1] ? 'bg-[#7A0000]' : 'bg-[#00477A]'} transition-all ease-in-out duration-[400ms] delay-[100ms]`}
+                        ></div>
+
+                        <div 
+                        onClick={() => {
+                            setHistory(() => history.indexOf(2) == -1 ? [...history, 2] : history);
+                            setColor(arr => arr.map((obj, i) => (i == 2 ? true : obj)));
+                        }}
+                        className={`h-[16.52vw] w-[16.52vw] lg:h-[6.94vw] rounded-3xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] ${currColor[2] ? 'bg-[#7A0000]' : 'bg-[#00477A]'} transition-all ease-in-out duration-[400ms] delay-[100ms]`}
+                        ></div>
+
+                        <div 
+                        onClick={() => {
+                            setHistory(() => history.indexOf(3) == -1 ? [...history, 3] : history);
+                            setColor(arr => arr.map((obj, i) => (i == 3 ? true : obj)));
+                        }}
+                        className={`h-[16.52vw] w-[16.52vw] lg:h-[6.94vw] rounded-3xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] ${currColor[3] ? 'bg-[#7A0000]' : 'bg-[#00477A]'} transition-all ease-in-out duration-[400ms] delay-[100ms]`}
+                        ></div>
+
+                        <div 
+                        onClick={() => {
+                            setHistory(() => history.indexOf(4) == -1 ? [...history, 4] : history);
+                            setColor(arr => arr.map((obj, i) => (i == 4 ? true : obj)));
+                        }}
+                        className={`h-[16.52vw] w-[16.52vw] lg:h-[6.94vw] rounded-3xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] ${currColor[4] ? 'bg-[#7A0000]' : 'bg-[#00477A]'} transition-all ease-in-out duration-[400ms] delay-[100ms]`}
+                        ></div>
+
+                        <div 
+                        onClick={() => {
+                            setHistory(() => history.indexOf(5) == -1 ? [...history, 5] : history);
+                            setColor(arr => arr.map((obj, i) => (i == 5 ? true : obj)));
+                        }}
+                        className={`h-[16.52vw] w-[16.52vw] lg:h-[6.94vw] rounded-3xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] ${currColor[5] ? 'bg-[#7A0000]' : 'bg-[#00477A]'} transition-all ease-in-out duration-[400ms] delay-[100ms]`}
+                        ></div>
+
+                        <div 
+                        onClick={() => {
+                            setHistory(() => history.indexOf(6) == -1 ? [...history, 6] : history);
+                            setColor(arr => arr.map((obj, i) => (i == 6 ? true : obj)));
+                        }}
+                        className={`h-[16.52vw] w-[16.52vw] lg:h-[6.94vw] rounded-3xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] ${currColor[6] ? 'bg-[#7A0000]' : 'bg-[#00477A]'} transition-all ease-in-out duration-[400ms] delay-[100ms]`}
+                        ></div>
+
+                        <div 
+                        onClick={() => {
+                            setHistory(() => history.indexOf(7) == -1 ? [...history, 7] : history);
+                            setColor(arr => arr.map((obj, i) => (i == 7 ? true : obj)));
+                        }}
+                        className={`h-[16.52vw] w-[16.52vw] lg:h-[6.94vw] rounded-3xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] ${currColor[7] ? 'bg-[#7A0000]' : 'bg-[#00477A]'} transition-all ease-in-out duration-[400ms] delay-[100ms]`}
+                        ></div>
+
+                <div onClick={()=>{reset();}}    className='h-[16.52vw] w-[16.52vw] lg:h-[6.94vw] rounded-3xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#7A0000]'></div>
             </div>
         </div>
       )
 }
-
+  
 export default Home
--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import 'remixicon/fonts/remixicon.css';
 
 export const Nav = () => {
     const [active,setActive]=useState(false);
+    let navigate=useNavigate();
   return (
     <div className={`w-[100%] md:text-md ${active? 'h-[42.75vw]' :'h-[16.03vw]' }  md:h-[7.66vw] lg:h-[3.8vw] items-center   flex justify-end gap-12 md:py-2 lg:py-2  md:px-8 lg:px-8 lg:text-[1.25vw] md:text-[2.42vw] font-light tracking-wide   shadow-[0_0_4px_4px_rgba(0,0,0,0.25)]`}>
         <div className={`${active? 'hidden':''} mr-[4.58vw] w-[12.72vw] h-[7.37vw] md:hidden lg:hidden flex flex-col justify-between`} onClick={()=>{setActive((status)=>!status)}}>
@@ -11,20 +13,26 @@ export const Nav = () => {
             <div className='bg-[#D9D9D9] w-[100%] h-[1.78vw] rounded-lg'></div>
         </div>
 
+        {/* dropdown  */}
         <div className={`${active ?'':'hidden'} md:hidden lg:hidden   relative h-[100%] w-[100%]`}>
                 <div onClick={()=>{setActive(()=>{return false;})}} className=" cross h-[10.25vw]  w-[10.25vw] relative top-[5.59vw] left-[81.93vw]">
                         <div className=' absolute bg-[#D9D9D9] h-[1.78vw] w-[12.72vw]  -translate-x-[50%] -translate-y-[50%] top-1/2 left-1/2 rotate-45 rounded-lg'></div>
                         <div className=' absolute  bg-[#D9D9D9] h-[1.78vw] w-[12.72vw] -translate-x-[50%] -translate-y-[50%] top-1/2 left-1/2 -rotate-45 rounded-lg'></div>
                 </div>
                 <div className="navigation absolute top-[18.57vw] left-1/2 -translate-x-1/2  flex flex-col text-[4.58vw] justify-center items-center ">
-                    <div className='mb-[1vw]'>Home</div>
-                    <div className='mb-[1vw]'>Grid</div>
-                    <div>LinkedIn</div>
+                    <div onClick={() => {window.location.href = 'http://localhost:5173/about';  }} className='cursor-pointer mb-[1vw]'>Home</div>
+                    <div className='mb-[1vw] cursor-pointer'>Grid</div>
+                    <div   onClick={() => {window.location.href = 'https://www.linkedin.com';  }}>LinkedIn</div>
                 </div>
         </div>
-        <div className='hidden md:block lg:block' >Home</div>
-        <div className='hidden md:block lg:block'>Grid</div>
-        <div className='hidden md:block lg:block'>Linkedin</div>
+
+
+
+
+        {/* main larger screen visible only */}
+        <div  onClick={()=>{}} className='hidden cursor-pointer hover:font-semibold md:block lg:block' >Home</div>
+        <div className='hidden cursor-pointer md:block hover:font-semibold  lg:block'>Grid</div>
+        <div  onClick={() => {window.location.href = 'https://www.linkedin.com'; }} className='hover:font-semibold  hidden cursor-pointer md:block lg:block'>Linkdin</div>
 
     </div>
   )
