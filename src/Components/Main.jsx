044cfd11d5744f1e4b68460c709e031185583dc6
khantnandeesh <nandeeshkhant3205@gmail.com>
2025-01-22 15:43:12 +0530

    pixel perfect lg,md

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div className=' lg:h-[26.73vw] lg:w-[83.54vw]    lg:mt-[3.47vw] md:mt-[6.45vw]    mx-auto flex lg:justify-between   md:justify-between   md:flex-wrap md:gap-[2.419vw]  md:h-[99vw] md:w-[92.87vw]'>
+        <div className=' lg:h-[26.73vw] lg:w-[83.54vw]   lg:mt-[3.47vw] md:mt-[6.45vw]    mx-auto flex lg:flex-nowrap   md:justify-between   md:flex-wrap md:gap-[2.419vw]  md:h-[99vw] md:w-[92.87vw]'>
 
-            <div className='lg:h-[26.73vw] lg:w-[26.73vw] lg:max-w-[26.73vw] lg:max-h-[26.73vw]  md:w-[100%] md:h-[51.74vw] bg-gradient-to-br rounded-lg from-[#3300FF] to-[#FA00FF] shadow-[0_0_4px_4px_rgba(0,0,0,0.25)]' ></div>
-            <div className='lg:h-[26.73vw] lg:w-[26.73vw] lg:max-w-[26.73vw] lg:max-h-[26.73vw]   md:h-[45.29vw] md:w-[45.29vw]  rounded-md overflow-hidden border-black border-[1px] bg-[#F7F7F7]'>
+            <div className='lg:h-[26.73vw] lg:w-[26.73vw] lg:shrink-0 lg:max-w-[26.73vw] lg:max-h-[26.73vw]  md:w-[100%] md:h-[51.74vw] bg-gradient-to-br rounded-lg from-[#3300FF] to-[#FA00FF] shadow-[0_0_4px_4px_rgba(0,0,0,0.25)]' ></div>
+            <div className='lg:h-[26.73vw] lg:w-[26.73vw] lg:shrink-0 lg:max-w-[26.73vw] lg:max-h-[26.73vw]   md:h-[45.29vw] md:w-[45.29vw]  rounded-md overflow-hidden border-black border-[1px] bg-[#F7F7F7]'>
                 <img src="https://s3-alpha-sig.figma.com/img/f451/3151/c543be76d658fa7cf401125e9cef6f94?Expires=1738540800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Umhrmf3ve5VwHjU9x-4lqn9VoteG~xmqFFAJ1NFlSAPT2F1K~eCqdTncMldvRuSwe7MGXpMkWCK9BVDlwoLxeKQ8MOk22FzwjwYl~dXpuh2U35tx1RvrkuHzRks0J7qpvRw~3LguA3Xi4mQjmUtCMqf9V7DbbaYO38HIqgj4iJ1JJ6u05G6GZZI5IqORNTSqkpUoRjfh1YuRICXgk79qz0718w~phQ2SE3Bmh1NCvyTjFecnU6ajWd6Bjj-mQ0gqKBCsaTsYA-X9YzyKyRkyCY6r8bDTCU43ejpA3U6bhERIx1CEgfUCQwTsnlKkwvdY0R56TLm38CkTc38cvbgyqQ__" className='h-full w-full  object-contain' alt="image" />
             </div>
 
-            <div className='lg:h-[26.73vw] lg:max-w-[26.73vw] lg:max-h-[26.73vw]  lg:w-[26.73vw] md:w-[45.026vw] md:h-[45.29vw] md:w-max-[45.026vw] md:h-max-[45.29vw]  lg:gap-[0.486vw] md:gap-[0.82vw]  bg-[#D9D9D9] rounded-lg lg:px-[2.465vw] lg:py-[2.396vw] flex justify-center items-center flex-wrap  shadow-[inset_0_0_4px_2px_rgba(0,0,0,0.25)]   md:px-[4.15vw] md:py-[4.06vw]'>
+            <div className='lg:h-[26.73vw] lg:shrink-0 lg:max-w-[26.73vw] lg:max-h-[26.73vw]  lg:w-[26.73vw] md:w-[45.026vw] md:h-[45.29vw] md:w-max-[45.026vw] md:h-max-[45.29vw]  lg:gap-[0.486vw] md:gap-[0.82vw]  bg-[#D9D9D9] rounded-lg  flex justify-center items-center flex-wrap  shadow-[inset_0_0_4px_2px_rgba(0,0,0,0.25)]   md:px-[4.15vw] md:py-[4.06vw] lg:px-[2.465vw] lg:py-[2.395vw]'>
                 <div  className='lg:h-[6.94vw] rounded-2xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]' ></div>
                 <div className='lg:h-[6.94vw] rounded-2xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]' ></div>
                 <div  className='lg:h-[6.94vw] rounded-2xl md:w-[11.69vw] md:h-[11.768vw] lg:w-[6.94vw] bg-[#00477A]'></div>
